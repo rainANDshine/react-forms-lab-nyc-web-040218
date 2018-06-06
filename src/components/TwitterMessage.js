3718e5fa8377ae06a53373347bfa6544c41cf983
Shun Yao <ysmiracle@hotmail.com>
2018-06-06 03:08:05 +0000

    automatically backed up by learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,23 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: ''
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" maxLength={this.props.maxChars} onChange={this.handleChange} value={this.state.value}/>
+        <p>{this.props.maxChars - this.state.value.length}</p>
       </div>
     );
   }
